5991c30cbb6a045e1fd32fdc7feb6f981e5359a2
NicolasM <mrzdude1@gmail.com>
2026-05-16 17:44:32 -0400

    prefijo añadido a las rutas del backend

--- back-cloud/servicio2/main.py
+++ back-cloud/servicio2/main.py
@@ -5,19 +5,22 @@ from api.api_usuarios import api_usuarios
 from api.api_base_datos import api_conexion
 from api.api_carrito import api_carrito
 
-app = FastAPI()
+# Al agregar root_path, FastAPI asume que el balanceador te manda 
+# el tráfico a través de ese prefijo.
+app = FastAPI(root_path="/api")
 
 print("esto se cambio")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Puertos donde corre tu Nginx
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# routers
+# Los routers se quedan exactamente igual, pero ahora 
+# responderán bajo /api/productos, /api/usuarios, etc.
 app.include_router(api_productos)
 app.include_router(api_usuarios)
 app.include_router(api_conexion)

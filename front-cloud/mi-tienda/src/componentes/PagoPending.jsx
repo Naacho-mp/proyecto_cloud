f4ae06b87a11b2c7b46432223dbcae95fea18b8f
Nacho <nachomatamala@gmail.com>
2026-07-06 21:09:44 -0400

    test

--- front-cloud/mi-tienda/src/componentes/PagoPending.jsx
+++ front-cloud/mi-tienda/src/componentes/PagoPending.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsClockHistory } from 'react-icons/bs';
-
+//test
 const PagoPending = () => {
   const navigate = useNavigate();
 
